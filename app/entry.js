b840038e24c6200019a80b067ea0165cb7a04e50
sano-suguru <sa.50.00.no.riku+github@gmail.com>
2018-10-02 11:20:23 +0000

    jqueryによるDOM操作を追加

--- app/entry.js
+++ app/entry.js
@@ -1,8 +1,15 @@
 'use strict';
-import dc from 'damage-calc';
-import crypto from 'crypto';
-document.writeln(
-  '<p>攻撃力 100, 防御 50, 防御貫通 30 のダメージは、' +
-  dc.effectiveDamage(100, 50, 30) + '</p>');
-document.writeln(
-  '<p>' + crypto.randomBytes(8).toString('hex') + '</p>');
+import $ from 'jquery';
+const block = $('#block');
+const scalingButton = $('#scaling-button');
+
+scalingButton.click(() => {
+  block.animate({
+    width: '200pt',
+    height: '200pt'
+  }, 2000);
+  block.animate({
+    width: '100pt',
+    height: '100pt'
+  }, 2000);
+})
